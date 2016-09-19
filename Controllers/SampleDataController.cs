3cd72a48b8a982be5ce64f5fc648c064438076b0
veerprabakar <prabakar.veer@gmail.com>
2016-09-20 03:39:14 +0800

    Change 1

---
+++ Controllers/SampleDataController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QuadFace.Models;
+
+namespace QuadFace.Controllers
+{
+    public class SampleDataController : Controller
+    {
+        private SampleContext db = new SampleContext();
+
+        // GET: SampleData
+        public ActionResult Index()
+        {
+            return View(db.SampleDatas.ToList());
+        }
+
+        // GET: SampleData/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SampleData sampleData = db.SampleDatas.Find(id);
+            if (sampleData == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sampleData);
+        }
+
+        // GET: SampleData/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SampleData/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Age,DataOfBirth,Email,Address")] SampleData sampleData)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SampleDatas.Add(sampleData);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(sampleData);
+        }
+
+        // GET: SampleData/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SampleData sampleData = db.SampleDatas.Find(id);
+            if (sampleData == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sampleData);
+        }
+
+        // POST: SampleData/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Age,DataOfBirth,Email,Address")] SampleData sampleData)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sampleData).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(sampleData);
+        }
+
+        // GET: SampleData/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SampleData sampleData = db.SampleDatas.Find(id);
+            if (sampleData == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sampleData);
+        }
+
+        // POST: SampleData/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SampleData sampleData = db.SampleDatas.Find(id);
+            db.SampleDatas.Remove(sampleData);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/SampleData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace QuadFace.Models
+{
+    public class SampleData
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50, ErrorMessage = "Name cannot be longer than 40 characters.")]
+        public string Name { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DataOfBirth { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+    }
+}
